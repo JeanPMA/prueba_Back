3e350796de3e9f94e6f24d5f4dce454291185707
Jhony Veizaga S <39717129+JhonyVS@users.noreply.github.com>
2021-10-18 18:06:45 -0400

    Add files via upload

---
+++ src/main/java/api/restfull/Betasoft/demo/BetasoftApplication.java
@@ -0,0 +1,13 @@
+package api.restfull.Betasoft.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetasoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetasoftApplication.class, args);
+	}
+
+}
